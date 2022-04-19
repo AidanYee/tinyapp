a663a581c30a9bfca5c92d4682c4cd05d3370dbb
Aidan Yee <aidan.yee@pm.me>
2022-04-18 18:06:22 -0700

    add route for submission box

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8081; //defult port 8080
+const PORT = 8081; // Default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,28 +9,45 @@ const urlDatabase = {
 
 app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {
-  res.send("Hello!");
-});
+// Routes
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/", (req,res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-  res.render("urls_show", templateVars);
+// app.get("/urls/:shortURL", (req, res) => {
+//   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+//   res.render("urls_show", templateVars);
+// });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  urlDatabase[req.body.name] = req.body.name;
+  urlDatabase[req.body.longURL] = req.body.longURL;
+  res.send('Ok');
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tinyapp listening on port ${PORT}!`);
 });
